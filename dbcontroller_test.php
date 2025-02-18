2e07a9b37c8f991e79e24bc6ceeb29a54cb4cb87
steyerzalan <steyerzalan@gmail.com>
2025-02-18 11:00:13 +0100

    dbcontroller letesztelve

--- dbcontroller_test.php
+++ dbcontroller_test.php
@@ -10,9 +10,28 @@ $db=new DBController();
 //teszt kapcsolat (connection)
 if ($db->connectDB())
 {
-    echo "Az adatbázis apcsolat sikeres <br>";
+    echo "Az adatbázis kapcsolat sikeres <br>";
 }
 else
 {
     echo "Az adatbázis apcsolat sikerTELEN <br>";
 }
+//Teszt Select query
+$query="SELECT * FROM fishingrod";
+$eredmeny=$db->executeSelectQuery($query);
+
+if($eredmeny !== "hiba")
+{
+    echo "Lekérdezés sieres! <br>";
+    echo "<pre>";
+    print_r($eredmeny);
+    echo "</pre>";
+}
+else
+{
+    print_r($eredmeny);
+    echo "Lekérdezés hiba <br>";
+}
+
+$db->closeDB();
+echo "Lezárva";
